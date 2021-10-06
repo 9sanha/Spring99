4be1646582018f52507519ca376011bb64dbb5f2
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-07 02:30:20 +0900

    Update SignupRequestDto.java

--- myBlog/src/main/java/com/saname/myblog/dto/SignupRequestDto.java
+++ myBlog/src/main/java/com/saname/myblog/dto/SignupRequestDto.java
@@ -2,13 +2,30 @@ package com.saname.myblog.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @Setter
 @Getter
+@ToString
 public class SignupRequestDto {
-    private String username;
+    private Long id;
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    private String nickname;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
-    private boolean admin = false;
-    private String adminToken = "";
+
 }
 
