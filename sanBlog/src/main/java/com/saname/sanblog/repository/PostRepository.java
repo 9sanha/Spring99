9282f2eb6cf4e79b2f646402eebd45893cc91371
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-08 01:30:00 +0900

    d
    z

--- sanBlog/src/main/java/com/saname/sanblog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saname.sanblog.repository;
-
-import com.saname.sanblog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
