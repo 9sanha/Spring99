118b476f143136a845302c377c3f9f3a9dd928f6
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-11 02:06:54 +0900

    amen

--- amen/src/main/java/com/saname/amen/repository/ReplyRepository.java
+++ amen/src/main/java/com/saname/amen/repository/ReplyRepository.java
@@ -4,8 +4,6 @@ import com.saname.amen.model.Post;
 import com.saname.amen.model.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReplyRepository extends JpaRepository<Reply,Long> {
     void deleteAllByPost(Post post);
 
